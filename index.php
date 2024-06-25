3ee312ad57aff62ad6d6ad4881b1c262ddd052c0
MatteoNeri99 <matteolavoro99@gmail.com>
2024-06-25 16:33:29 +0200

    aggiunta variabile $inno

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+$inno="Fratelli d'Italia,
+l'Italia s'è desta,
+dell'elmo di Scipio
+s'è cinta la testa.
+Dov'è la Vittoria?
+Le porga la chioma,
+ché schiava di Roma
+Iddio la creò.
+Fratelli d'Italia,
+l'Italia s'è desta,
+dell'elmo di Scipio
+s'è cinta la testa.
+Dov'è la Vittoria?
+Le porga la chioma,
+ché schiava di Roma
+Iddio la creò.
+Stringiamci a coorte,
+siam pronti alla morte.
+Siam pronti alla morte,
+l'Italia chiamò.
+Stringiamci a coorte,
+siam pronti alla morte.
+Siam pronti alla morte,
+l'Italia chiamò!"
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<p> <?php echo $inno ?></p>
+    
+</body>
+</html>
